ea285ba8f21d775d0ed82284d6180985668a3a00
Riku Ohshiro <e195763@ie.u-ryukyu.ac.jp>
2019-12-09 12:23:40 +0900

    レポート４のステップ1

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        try {
+//        try {
             String str = null;
             System.out.println(str.length());
-        } catch (NullPointerException e){
-            System.out.println("NullPointerExceptionが発生");
-            System.out.print(e.getMessage());
-        }
+//        } catch (NullPointerException e) {
+//            System.out.println("NullPointerExceptionが発生");
+//            System.out.print(e.getMessage());
+//        }
     }
 }
