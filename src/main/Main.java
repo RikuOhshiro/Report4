3afb51dcee205c9ee14fabbde744a58c84a927cd
Riku Ohshiro <e195763@ie.u-ryukyu.ac.jp>
2019-12-09 12:38:23 +0900

    レポート４のステップ3

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-            String str = null;
-            System.out.println(str.length());
+            String str = "百二十三";
+            int value = Integer.parseInt(str);
     }
 }
